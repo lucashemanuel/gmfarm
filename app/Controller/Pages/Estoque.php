c072b5c07a39193a9ae00559cf4d33f38fbb848f
Rubens Júnior <rubens.jr107@gmail.com>
2022-05-21 22:27:25 -0300

    created insert of items at estoque

--- app/Controller/Pages/Estoque.php
+++ app/Controller/Pages/Estoque.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Pages;
 
 use \App\Utils\View;
-use \App\Model\Entity\Organization;
+use \App\Model\Entity\Estoque as EntityEstoque;
 
 /* O Controlador vai ser responsável por receber uma ação, executar o Model para obter os dados necessários e depois passar os dados para a View */
 
@@ -14,13 +14,43 @@ use \App\Model\Entity\Organization;
 class Estoque extends Page
 {
 
+  /**
+   * Método responsável por obter a renderização dos itens de estoque 
+   * @return string
+   */
+  private static function getEstoqueItems(){
+    /* MATERIAIS */
+    $itens = '';
+
+    /* Resultados da página */
+    $results = EntityEstoque::getItems(null, 'id_material DESC');
+
+    /* Renderiza o item */
+    while($obEstoque = $results->fetchObject(EntityEstoque::class)){
+      $itens .= View::render('pages/estoque/item', [
+        'id_material' => $obEstoque->id_material,
+        'reagente' => $obEstoque->reagente,
+        'lote' => $obEstoque->lote,
+        'fabricante' => $obEstoque->fabricante,
+        'fabricacao' => date('m/Y', strtotime($obEstoque->fabricacao)),
+        'validade' => date('m/Y', strtotime($obEstoque->validade)),
+        'quantidade' => $obEstoque->quantidade,
+        'embalagem_original' => $obEstoque->embalagem_original,
+        'cas' => $obEstoque->cas,
+      ]);
+    }
+
+    /* RETORNA OS ITENS */
+    return $itens;
+  }
+
   public static function getEstoque()
   {
-    $obOrganization = new Organization;
 
     /* View do Estoque */
     $content = View::render('pages/estoque', [
-      /* CONTENT */]);
+      'itens' => self:: getEstoqueItems()
+    ]);
 
     /* Retorna a View da Página */
     return parent::getPage('ESTOQUE > GMFARM', $content);
@@ -31,10 +61,18 @@ class Estoque extends Page
 
     /* DADOS DO POST */
     $postVars = $request->getPostVars();
-    echo "<pre>";
-    print_r($postVars);
-    echo "</pre>";
-    exit;
+
+    /* NOVA INSTANCIA DE ESTOQUE */
+    $obEstoque = new EntityEstoque;
+    $obEstoque->reagente = $postVars['reagente'];
+    $obEstoque->lote = $postVars['lote'];
+    $obEstoque->fabricante = $postVars['fabricante'];
+    $obEstoque->fabricacao = $postVars['fabricacao'];
+    $obEstoque->validade = $postVars['validade'];
+    $obEstoque->quantidade = $postVars['quantidade'];
+    $obEstoque->embalagem_original = $postVars['embalagem_original'];
+    $obEstoque->cas = $postVars['cas'];
+    $obEstoque->cadastrar();
 
     return self::getEstoque();
   }
--- app/Model/Entity/Estoque.php
+++ app/Model/Entity/Estoque.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Model\Entity;
+use \WilliamCosta\DatabaseManager\Database;
 
 class Estoque
 {
@@ -9,13 +10,15 @@ class Estoque
 
   public $reagente;
 
+  public $lote;
+
   public $fabricante;
 
   public $fabricacao;
 
   public $validade;
 
-  public $qtd;
+  public $quantidade;
 
   public $embalagem_original;
 
@@ -23,9 +26,32 @@ class Estoque
 
   public function cadastrar()
   {
-    echo "<pre>";
-    print_r($this);
-    echo "</pre>";
-    exit;
+    /* INSERE NO BANCO DE DADOS */
+    $this->id_material = (new Database('estoque'))->insert([
+      'reagente' => $this->reagente,
+      'lote' => $this->lote,
+      'fabricante' => $this->fabricante,
+      'fabricacao' => $this->fabricacao,
+      'validade' => $this->validade,
+      'quantidade' => $this->quantidade,
+      'embalagem_original' => $this->embalagem_original,
+      'cas' => $this->cas
+    ]);
+
+    /* Sucesso */
+    return true;
+  }
+
+  /**
+   * Método responsável por retornar (itens do estoque)
+   * @param string $where
+   * @param string $order
+   * @param string $limit
+   * @param string $fields
+   * @return PDOStatement 
+   */ 
+  public static function getItems($where = null, $order = null, $limit = null, $fields = '*'){
+
+    return (new Database('estoque'))->select($where, $order, $limit, $fields);
   }
 }
