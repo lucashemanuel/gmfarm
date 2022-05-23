9729cb88aa3f8199f84b29acf436c40971b62154
Rubens Júnior <rubens.jr107@gmail.com>
2022-05-23 16:01:47 -0300

    delete estoque get and post

--- routes/pages.php
+++ routes/pages.php
@@ -11,20 +11,6 @@ $obRouter->get('/', [
   }
 ]);
 
-/* Rota ESTOQUE */
-$obRouter->get('/estoque', [
-  function ($request) {
-    return new Response(200, Pages\Estoque::getEstoque($request));
-  }
-]);
-
-/* Rota ESTOQUE (INSERT)*/
-$obRouter->post('/estoque', [
-  function ($request) {
-    return new Response(200, Pages\Estoque::insertMaterial($request));
-  }
-]);
-
 /* Rotas DINAMICAS */
 $obRouter->get('/pagina/{idPagina}/{acao}', [
   function ($idPagina, $acao) {
