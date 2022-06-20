189b2df30abc43385e1b8f22adca0633bbf684d8
Rubens Júnior <rubens.jr107@gmail.com>
2022-06-19 21:05:34 -0300

    updated sql

--- gmfarm.sql
+++ gmfarm.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 23-Maio-2022 às 10:21
--- Versão do servidor: 8.0.29-0ubuntu0.22.04.2
--- versão do PHP: 8.1.2
+-- Generation Time: Jun 19, 2022 at 09:04 PM
+-- Server version: 8.0.29-0ubuntu0.22.04.2
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,15 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `gmfarm`
+-- Database: `gmfarm`
 --
+CREATE DATABASE IF NOT EXISTS `gmfarm` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `gmfarm`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `estoque`
+-- Table structure for table `estoque`
 --
 
 CREATE TABLE `estoque` (
@@ -37,69 +39,82 @@ CREATE TABLE `estoque` (
   `embalagem_original` varchar(10) NOT NULL,
   `quantidade` int NOT NULL,
   `cas` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Extraindo dados da tabela `estoque`
---
-
-INSERT INTO `estoque` (`id_material`, `reagente`, `lote`, `fabricante`, `fabricacao`, `validade`, `embalagem_original`, `quantidade`, `cas`) VALUES
-(2, 'teste', 'teste', 'teste', '2022-01', '2022-02', 'Não', 200, 'asdasdawsd'),
-(3, 'sadsad', 'asdasd', 'asdasd', '2022-04', '2022-12', 'Não', 400, 'asdasdawsd'),
-(5, 'dsadasd', 'asdas', 'asdasdasd', '2022-08', '2022-12', 'Não', 500, 'asdasdawsd');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuarios`
+-- Table structure for table `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `id` int NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contato` varchar(20) NOT NULL,
+  `genero` varchar(5) NOT NULL,
+  `situacao` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `id` int NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `matricula` varchar(20) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_usuario` varchar(10) NOT NULL DEFAULT 'aluno'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
-(1, 'Admin', 'admin@teste.com', '$2y$10$kB8.xkqkOsveDBo4CXQEIeWV/KDVjUEgi7xnGC9yA9XOz15QdY58O');
-
---
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `estoque`
+-- Indexes for table `estoque`
 --
 ALTER TABLE `estoque`
   ADD PRIMARY KEY (`id_material`);
 
 --
--- Índices para tabela `usuarios`
+-- Indexes for table `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `estoque`
+-- AUTO_INCREMENT for table `estoque`
 --
 ALTER TABLE `estoque`
-  MODIFY `id_material` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_material` int NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `usuarios`
+-- AUTO_INCREMENT for table `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
